f8daeca82e9118d6b5fc91d56b3c0d19f197bb01
Hanne-Grete Alvheim <hannegga@stud.ntnu.no>
2021-01-28 20:35:29 +0100

    Did exercise four for foo.c and foo.go

--- c/foo.c
+++ c/foo.c
@@ -8,17 +8,26 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
-    return NULL;
+   for (int inc = 0; inc < 1000000; inc++){
+		i++; 
+	}
+ return NULL;
+
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+	for (int inc = 0; inc < 1000000; inc++){
+		i--;
+	}	
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+	pthread_t incrementingThread;
+	pthread_t decrementingThread;  	
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
@@ -28,3 +37,4 @@ int main(){
     printf("The magic number is: %d\n", i);
     return 0;
 }
+
